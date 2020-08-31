4a8d142c31751844018e273ef383a75cef29c2c2
Vadim <65212516+VadimChorrny@users.noreply.github.com>
2020-08-31 17:32:38 +0300

    workplace settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Style/Style.css";
-import FrontPage from "./Pages/frontPage";
 import Index from "./Pages/index";
 
 function App() {
     return (
-        <div className="App">
+        <div>
             <Index />
         </div>
     );
